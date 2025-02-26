e89e8c40979adfe4e9393b4bff26578b75fd9cec
MateoNihil <mateo.nihil@gmail.com>
2025-02-25 23:23:14 -0500

    v1 - MemoryFragmentTabs + RawFunctionalTab1

--- app/src/main/java/zzz/master/general/ui/memory/MemoryViewModel.java
+++ app/src/main/java/zzz/master/general/ui/memory/MemoryViewModel.java
@@ -1,19 +1,7 @@
 package zzz.master.general.ui.memory;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MemoryViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
-
-    public MemoryViewModel() {
-        mText = new MutableLiveData<>();
-        //mText.setValue("This is memory fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
